82e50df3b9a9c9a432a393138b5497ad2ad07ee4
Jeff Principe <princjef@gmail.com>
2014-05-02 23:58:40 -0400

    finished scrape of information from google voice and population into list

--- data/panel.js
+++ data/panel.js
@@ -1,7 +1,16 @@
 var classNames = {
   conversation: "contact",
   detailsView: "left-shifted",
-  placeholder: "grey"
+  placeholder: "grey",
+  gvConversation: 'gc-message',
+  gvConversationRead: 'gc-message-read',
+  gvConversationUnread: 'gc-message-unread',
+  gvConversationPicture: 'gc-message-portrait',
+  gvConversationContactName: 'gc-message-name-link',
+  gvMessage: 'gc-message-sms-row',
+  gvMessageFrom: 'gc-message-sms-from',
+  gvMessageContent: 'gc-message-sms-text',
+  gvMessageTime: 'gc-message-sms-time'
 };
 
 var ids = {
@@ -53,45 +62,83 @@ var handlers = {
 };
 
 var dom = {
-  populateConversationListing: function(json) {
+  parseListingHTML: function(htmlString, jsonResponse) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(htmlString, "text/html");
+    var json = [];
+
+    var conversations = doc.getElementsByClassName(classNames.gvConversation);
+    for (var i = 0; i < conversations.length; i++) {
+      var id = conversations[i].id;
+      var conversation = {
+        id: id,
+        isRead: conversations[i].classList.contains(classNames.gvConversationRead),
+        timestamp: jsonResponse[id].displayStartDateTime,
+        relativeTime: jsonResponse[id].relativeStartTime,
+        contact: {
+          name: conversations[i].getElementsByClassName(classNames.gvConversationContactName)[0].innerHTML,
+          displayNumber: jsonResponse[id].displayNumber,
+          phoneNumber: jsonResponse[id].phoneNumber,
+          imageUrl: conversations[i].getElementsByClassName(classNames.gvConversationPicture)[0].innerHTML
+        },
+        messages: []
+      };
+
+      var messages = conversations[i].getElementsByClassName(classNames.gvMessage);
+      for (var j = 0; j < messages.length; j++) {
+        var message = {
+          isOutgoing: messages[j].getElementsByClassName(classNames.gvMessageFrom)[0].innerHTML.trim() === "Me:",
+          content: messages[j].getElementsByClassName(classNames.gvMessageContent)[0].innerHTML,
+          time: messages[j].getElementsByClassName(classNames.gvMessageTime)[0].innerHTML
+        };
+        conversation.messages.push(message);
+      }
+
+      json.push(conversation);
+    }
+    return json;
+  },
+  populateConversationListing: function(conversations) {
     var list = document.getElementById(ids.conversationListing);
-    for (var id in json.messages) {
+    list.innerHTML = "";
+    conversations.forEach(function(conversation) {
       var contact = document.createElement('li');
 
       // Classes
       contact.classList.add('contact');
-      if (json.messages[id].isRead) {
+      if (conversation.isRead) {
         contact.classList.add('read');
       } else {
         contact.classList.add('unread');
       }
 
       // Data attributes
-      contact.dataset.id = id;
-      contact.dataset.phone = json.messages[id].displayNumber;
+      contact.dataset.id = conversation.id;
+      contact.dataset.phone = conversation.contact.displayNumber;
 
       // Name
       var name = document.createElement('div');
       name.classList.add('contact-name');
-      name.innerHTML = "No name yet :(";
+      name.innerHTML = conversation.contact.name;
       contact.appendChild(name);
 
       // Phone Number
       var phoneNumber = document.createElement('div');
       phoneNumber.classList.add('small');
       phoneNumber.classList.add('grey');
-      phoneNumber.innerHTML = json.messages[id].displayNumber;
+      phoneNumber.innerHTML = conversation.contact.displayNumber;
       contact.appendChild(phoneNumber);
 
       list.appendChild(contact);
-    }
+    });
   }
 };
 
 var api = {
   smsListing: function() {
-    self.port.on('recentSMS', function(responseJSON) {
-      dom.populateConversationListing(responseJSON);
+    self.port.on('recentSMS', function(response) {
+      var conversations = dom.parseListingHTML(response.html, response.json);
+      dom.populateConversationListing(conversations);
       handlers.conversationClick();
     });
   }
