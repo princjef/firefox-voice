2f5ddb8228ade76279d1632820631a174e486442
Jeff Principe <princjef@gmail.com>
2014-05-07 23:16:19 -0400

    factored out typeahead

--- data/panel.js
+++ data/panel.js
@@ -100,116 +100,6 @@ var handlers = {
       document.getElementById(ids.wrap).classList.toggle("find-contact");
       document.getElementById(ids.newConversationInput).focus();
     });
-  },
-  contactTypeahead: function() {
-    var contactInput = document.getElementById(ids.newConversationInput);
-    var typeaheadContacts = document.getElementsByClassName(classNames.contactTypeaheadElement);
-
-    var typeahead = function() {
-      window.setTimeout(function() {
-        var value = contactInput.value.toLowerCase();
-        if (value !== globals.oldContactInputValue) {
-          globals.oldContactInputValue = value;
-          var numValue = value.replace(/\.|-|\s/g, "");
-          var firstElement = true;
-
-          for (var i = 0; i < typeaheadContacts.length; i++) {
-            var contact = typeaheadContacts.item(i);
-            if (contact.dataset.name.toLowerCase().contains(value) ||
-                contact.dataset.phoneNumber.contains(numValue) ||
-                contact.dataset.displayNumber.contains(value)
-            ) {
-              contact.classList.remove('hidden');
-              contact.classList.add('visible');
-              if (firstElement) {
-                contact.classList.add('selected');
-                firstElement = false;
-              } else {
-                contact.classList.remove('selected');
-              }
-            } else {
-              contact.classList.add('hidden');
-              contact.classList.remove('visible');
-              contact.classList.remove('selected');
-            }
-          }
-        }
-      }, 0);
-    };
-
-    contactInput.addEventListener('change', typeahead);
-    contactInput.addEventListener('cut', typeahead);
-    contactInput.addEventListener('paste', typeahead);
-    contactInput.addEventListener('drop', typeahead);
-    contactInput.addEventListener('keydown', typeahead);
-  },
-  arrowKeys: function() {
-    document.getElementById(ids.newConversationInput).addEventListener('keydown', function(event) {
-      var getSelectedIndex = function(elements) {
-        for (var i = 0; i < elements.length; i++) {
-          if (elements.item(i).classList.contains('selected')) {
-            return i;
-          }
-        }
-        return null;
-      };
-
-      var selectedElement = null;
-
-      if (event.keyCode === 38 || event.keyCode === 40) {  // Up/down Arrow
-        var visibleElements = document.querySelectorAll('.typeahead-element.visible');
-        var index = getSelectedIndex(visibleElements);
-        if (index !== null) {
-          visibleElements.item(index).classList.remove('selected');
-        }
-
-        var newIndex;
-        if (event.keyCode === 38) {  // Up arrow
-          newIndex = ((index === null || index <= 0) ? (visibleElements.length - 1) : (index - 1));
-        } else {  // Down arrow
-          newIndex = ((index === null || index >= visibleElements.length - 1) ? 0 : (index + 1));
-        }
-
-        visibleElements.item(newIndex).classList.add('selected');
-        selectedElement = visibleElements.item(newIndex);
-        event.stopPropagation();
-        event.preventDefault();
-
-      } else if (event.keyCode === 13) { // Enter
-        var selectedElement = document.querySelectorAll('.typeahead-element.visible.selected')[0];
-        api.selectContact(selectedElement);
-      }
-
-      if (selectedElement !== null) {
-        var typeahead = document.getElementById(ids.contactTypeaheadWrap);
-        // console.log("OutScroll", typeahead.scrollTop, "OutHeight", typeahead.offsetHeight, "InScroll", selectedElement.offsetTop, "InHeight", selectedElement.offsetHeight);
-        if (selectedElement.offsetTop < typeahead.scrollTop) {
-          var scroll = selectedElement.offsetTop;
-          typeahead.scrollTop = scroll;
-        } else if (selectedElement.offsetTop + selectedElement.offsetHeight > typeahead.scrollTop + typeahead.offsetHeight) {
-          var scroll = (selectedElement.offsetTop + selectedElement.offsetHeight - typeahead.offsetHeight);
-          typeahead.scrollTop = scroll;
-        }
-      }
-    });
-  },
-  typeaheadSelect: function() {
-    console.log("Called typeaheadSelect");
-    var typeaheadElements = document.getElementsByClassName(classNames.contactTypeaheadElement);
-
-    console.log("Typeahead Elements Length", typeaheadElements.length);
-    for (var i = 0; i < typeaheadElements.length; i++) {
-      typeaheadElements.item(i).addEventListener('mouseenter', function(event) {
-        for (var j = 0; j < typeaheadElements.length; j++) {
-          typeaheadElements.item(j).classList.remove('selected');
-        }
-        this.classList.add('selected');
-      });
-
-      typeaheadElements.item(i).addEventListener('click', function(event) {
-        api.selectContact(this);
-      });
-    }
   }
 };
 
@@ -377,34 +267,6 @@ var dom = {
         time.innerHTML = "Failed to send";
       }
     };
-  },
-  populateTypeahead: function() {
-    var typeahead = document.getElementById(ids.contactTypeahead);
-    typeahead.innerHTML = "";
-
-    globals.user.rankedContacts.forEach(function(contactId) {
-      var contactInfo = globals.user.contacts[contactId];
-      var contact = document.createElement('li');
-      contact.classList.add('typeahead-element');
-      contact.classList.add('visible');
-      contact.dataset.contactId = contactInfo.contactId;
-      contact.dataset.phoneNumber = contactInfo.phoneNumber;
-      contact.dataset.displayNumber = contactInfo.displayNumber;
-      contact.dataset.name = contactInfo.name;
-
-      var name = document.createElement('span');
-      name.innerHTML = contactInfo.name + " ";
-      contact.appendChild(name);
-
-      var phone = document.createElement('span');
-      phone.classList.add('grey');
-      phone.innerHTML = contactInfo.displayNumber;
-      contact.appendChild(phone);
-
-      typeahead.appendChild(contact);
-    });
-
-    handlers.typeaheadSelect();
   }
 };
 
@@ -443,7 +305,7 @@ var api = {
         .replace(/\,\s*\]/g, "]");     // get rid of trailing commas in array
       globals.user = JSON.parse(userText);
       console.log("Extracted user info");
-      dom.populateTypeahead();
+      typeahead.populate();
     });
   },
   updateMessageTime: function() {
@@ -487,35 +349,6 @@ var api = {
       document.getElementById(ids.wrap).classList.add(classNames.detailsView);
       dom.populateConversationDetails(data.id);
     });
-  },
-  selectContact: function(element) {
-    var contactId = element.dataset.contactId;
-    var key = "";
-    for (var id in globals.conversations) {
-      if (globals.conversations[id].contact.phoneNumber === element.dataset.phoneNumber) {
-        key = id;
-        break;
-      }
-    }
-
-    if (key === "") {
-      globals.conversations[""] = {
-        id: "",
-        isRead: true,
-        timestamp: "",
-        relativeTime: "",
-        contact: {
-          name: element.dataset.name,
-          displayNumber: element.dataset.displayNumber,
-          phoneNumber: element.dataset.phoneNumber,
-          imageUrl: ""
-        },
-        messages: []
-      };
-    }
-
-    dom.populateConversationDetails(key);
-    document.getElementById(ids.wrap).classList.add(classNames.detailsView);
   }
 };
 
@@ -524,8 +357,8 @@ window.onload = function() {
   handlers.sendMessage();
   handlers.messageInputChange();
   handlers.newConversationClick();
-  handlers.contactTypeahead();
-  handlers.arrowKeys();
+  typeahead.inputChange();
+  typeahead.keyEvents();
   api.smsListing();
   api.globalData();
   api.updateMessageTime();
